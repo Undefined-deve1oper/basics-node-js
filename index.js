a4195dfd0c88f858f3652fe94769db29b4d78331
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-01-29 15:45:38 +0600

    added a deletion method

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const yargs = require("yargs");
 const pkg = require("./package.json");
+const { addNote, printNotes, removeNote } = require("./notes.controller");
 
 yargs.version(pkg.version);
 
@@ -9,25 +10,35 @@ yargs.command({
     builder: {
         title: {
             type: "string",
-            describe: "Note title"
+            describe: "Note title",
+            demandOption: true
         }
     },
     handler({ title }) {
-        console.log("Add command: ", title);
+        addNote(title);
+    }
+});
+
+yargs.command({
+    command: "remove",
+    describe: "Remove note by id",
+    builder: {
+        id: {
+            type: "string",
+            describe: "Note id",
+            demandOption: true
+        }
+    },
+    handler({ id }) {
+        removeNote(id);
     }
 });
 
 yargs.command({
     command: "list",
     describe: "Print all notes",
-    builder: {
-        title: {
-            type: "string",
-            describe: "Note title"
-        }
-    },
-    handler({ title }) {
-        console.log("Add command: ", title);
+    handler() {
+        printNotes();
     }
 });
 
---
+++ notes.controller.js
@@ -0,0 +1,48 @@
+const fs = require("fs/promises");
+const path = require("path");
+const chalk = require("chalk");
+
+const notesPath = path.join(__dirname, "db.json");
+
+async function addNote(title) {
+    const notes = await getNotes();
+
+    const note = {
+        title,
+        id: Date.now().toString()
+    };
+
+    notes.push(note);
+
+    console.log(chalk.bgYellow("The note has been added"));
+    await fs.writeFile(notesPath, JSON.stringify(notes));
+}
+
+async function removeNote(id) {
+    const notes = await getNotes();
+
+    const updatedNotes = notes.filter((note) => note.id !== id);
+
+    console.log(chalk.bgYellow("The note has been deleted"));
+    await fs.writeFile(notesPath, JSON.stringify(updatedNotes));
+}
+
+async function getNotes() {
+    const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+    return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
+}
+
+async function printNotes() {
+    const notes = await getNotes();
+    console.log(chalk.bgGray("Here is the list of notes"));
+
+    notes.forEach((note) => {
+        console.log(chalk.yellow(note.title));
+    });
+}
+
+module.exports = {
+    addNote,
+    printNotes,
+    removeNote
+};
